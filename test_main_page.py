5a112b36d504590c12191a938038be460e0ff104
GlebKaglar <kanafiev.gl@gmail.com>
2022-07-01 13:22:12 +0300

    added first Page Object test

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,16 @@
 # pytest -v --tb=line --language=en test_main_page.py
-
+from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/"
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, link)
+    page.open()              # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+
+def go_to_login_page(browser):
+    login_link = browser.find_element(By.ID, 'login_link')
     login_link.click()
