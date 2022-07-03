486a281f754c2287804b50b2e2a8666b022a02d9
GlebKaglar <kanafiev.gl@gmail.com>
2022-07-03 14:46:45 +0300

    add expected cinditions in quiz method

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,10 @@
+from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException
+import math
 import time
 
-from selenium.common.exceptions import NoSuchElementException
-import math
-
+from selenium.webdriver.support.wait import WebDriverWait  # (Проблема с accept)
+from selenium.common.exceptions import TimeoutException  # (Проблема с accept)
+from selenium.webdriver.support import expected_conditions as EC    # (Проблема с accept)
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
@@ -26,4 +28,20 @@ class BasePage():
             return False
         return True
 
-
+# Решаем пример и встваляем ответ в полве ввода модального окна
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        print('Answer = ', answer)
+        alert.accept()
+        print('Нажали на кнопку')
+        try:
+            WebDriverWait(self.browser, 5).until(EC.alert_is_present())  # Добавил неявное ожидание (Проблема с accept)
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except (NoAlertPresentException, TimeoutException):  # добавил TimeoutException
+            print("No second alert presented")
--- pages/product_page.py
+++ pages/product_page.py
@@ -13,22 +13,6 @@ class ProductPage(BasePage):
         print('Добавляем товар в корзину')
         time.sleep(3)
 
-    # Решаем пример и встваляем ответ в полве ввода модального окна
-    def solve_quiz_and_get_code(self):
-        alert = self.browser.switch_to.alert
-        x = alert.text.split(" ")[2]
-        answer = str(math.log(abs((12 * math.sin(float(x))))))
-        alert.send_keys(answer)
-        print(answer)
-        time.sleep(2)
-        alert.accept()
-        # time.sleep(2)
-        try:
-            alert = self.browser.switch_to.alert
-            alert_text = alert.text
-            print(f"Your code: {alert_text}")
-            alert.accept()
-        except NoAlertPresentException:
-            print("No second alert presented")
+
 
 
